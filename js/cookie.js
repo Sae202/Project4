c3c6fc9cd0d8e1fcd7da992253e4a339354a0e9d
Sae202 <sfa25@njit.edu>
2022-12-03 00:57:22 -0500

    Project 4 Sae Almeida

---
+++ js/cookie.js
@@ -0,0 +1,13 @@
+
+   /* setCookie=(cName, cValue, expDays) => {
+    let date= new Date();
+    date.setTime(date.setTime()+(expDays * 24 *60 *60 *1000) );
+    const expires="expires=" + date.toUTCString();
+    document.cookie= cName + "=" + cValue +";" + expires + "; path=/"
+    }
+    document.querySelector(cookies-btn).addEventListener("click", () => {
+    document.querySelector("#cookies").style.display="none"
+    setCookie("cookie", true, 10)
+}) */
+
+   /* This will remain as comment for now */
